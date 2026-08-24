be6c7fbf6d30b4050b343d2af6f4c900270b48e3
Phạm Thiện <phamthienx@gmail.com>
2026-08-24 20:53:06 +0700

    ss
    ss

---
+++ frontend/src/apps/Header/UpgradeButton.jsx
@@ -0,0 +1,34 @@
+import { Avatar, Popover, Button, Badge, Col, List } from 'antd';
+
+// import Notifications from '@/components/Notification';
+
+import { RocketOutlined } from '@ant-design/icons';
+
+import useLanguage from '@/locale/useLanguage';
+
+export default function UpgradeButton() {
+  const translate = useLanguage();
+
+  return (
+    <Badge count={1} size="small">
+      <Button
+        type="primary"
+        style={{
+          float: 'right',
+          marginTop: '5px',
+          cursor: 'pointer',
+        }}
+        icon={<RocketOutlined />}
+        onClick={() => {
+          window.open(`https://cloud.idurarapp.com`);
+        }}
+      >
+        {translate('Try Entreprise Version')}
+      </Button>
+    </Badge>
+  );
+}
+
+console.log(
+  '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+);
