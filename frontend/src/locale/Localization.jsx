9b9e2fff26e25a949fede84d7dbf58c53a739a94
Phạm Thiện <phamthienx@gmail.com>
2026-08-24 22:08:42 +0700

    s
    s

--- frontend/src/locale/Localization.jsx
+++ frontend/src/locale/Localization.jsx
@@ -1,17 +1,30 @@
+import { useState, useEffect } from 'react';
+
 import { ConfigProvider } from 'antd';
 
+import { useSelector } from 'react-redux';
+
+import { selectLangState } from '@/redux/translate/selectors';
+
+const importLangFile = async () => {
+  return await import('./antdLocale');
+};
+
 export default function Localization({ children }) {
+  const { langCode, langDirection } = useSelector(selectLangState);
+
+  const [locale, setLocal] = useState();
+  const [direction, setDirection] = useState();
+
+  useEffect(() => {
+    const antdLocale = importLangFile();
+    const lang = antdLocale[langCode];
+    setDirection(langDirection);
+    setLocal(lang);
+  }, [langCode]);
+
   return (
-    <ConfigProvider
-      theme={{
-        // algorithm: theme.darkAlgorithm,
-        token: {
-          colorPrimary: '#1640D6',
-          colorLink: '#1640D6',
-          borderRadius: 8,
-        },
-      }}
-    >
+    <ConfigProvider direction={direction} locale={locale}>
       {children}
     </ConfigProvider>
   );
--- frontend/src/locale/antdLocale.js
+++ frontend/src/locale/antdLocale.js
@@ -1,7 +1,31 @@
-import en_US from 'antd/es/locale/en_US';
+import frFR from 'antd/es/locale/fr_FR';
+import arEG from 'antd/es/locale/ar_EG';
+import zhCN from 'antd/es/locale/zh_CN';
+import roRO from 'antd/es/locale/ro_RO';
+import itIT from 'antd/es/locale/it_IT';
+import hiIN from 'antd/es/locale/hi_IN';
+import thTH from 'antd/es/locale/th_TH';
+import esES from 'antd/es/locale/es_ES';
+import deDE from 'antd/es/locale/de_DE';
+import ruRU from 'antd/es/locale/ru_RU';
+import trTR from 'antd/es/locale/tr_TR';
+import ptBr from 'antd/es/locale/pt_BR';
+import viVN from 'antd/es/locale/vi_VN';
 
 const antdLocale = {
-  en_us: en_US,
+  zh_cn: zhCN,
+  fr_fr: frFR,
+  ar_eg: arEG,
+  ro_ro: roRO,
+  it_it: itIT,
+  hi_in: hiIN,
+  th_th: thTH,
+  es_es: esES,
+  de_de: deDE,
+  ru_ru: ruRU,
+  tr_tr: trTR,
+  pt_pt: ptBr,
+  vi_vn: viVN,
 };
 
 export default antdLocale;
